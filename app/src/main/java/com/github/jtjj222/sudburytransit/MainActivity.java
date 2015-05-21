0781cd58c562c7696b39ee5534e7f2580ac76ea8
Justin Michaud <jtjj222@live.ca>
2015-05-20 21:08:18 -0400

    move bus stops activity to a fragment in preparation for nav drawer

---
+++ app/src/main/java/com/github/jtjj222/sudburytransit/MainActivity.java
@@ -0,0 +1,47 @@
+package com.github.jtjj222.sudburytransit;
+
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Placeholder for now, plan to replace this activity with a
+        // material design nav drawer
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new StopsMapFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/github/jtjj222/sudburytransit/StopsMapFragment.java
+++ app/src/main/java/com/github/jtjj222/sudburytransit/StopsMapFragment.java
@@ -1,30 +1,30 @@
 package com.github.jtjj222.sudburytransit;
 
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
 
-public class StopsMapActivity extends AppCompatActivity {
+public class StopsMapFragment extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stops_map);
+        View view = inflater.inflate(R.layout.fragment_stops_map, parent, false);
 
-        MapView map = (MapView) findViewById(R.id.map);
+        MapView map = (MapView) view.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(false);
@@ -32,41 +32,23 @@ public class StopsMapActivity extends AppCompatActivity {
 
         ArrayList<BusStopOverlayItem> items = new ArrayList<>();
 
+        //Some test data
         items.add(new BusStopOverlayItem("SudburyDowntown", "Downtown",
                 "Description", new GeoPoint(46.491271667182488, -80.988006619736623)));
         items.add(new BusStopOverlayItem("NorthCentralPark", "North Central Park",
                 "North of Central Park in New York City", new GeoPoint(46.493646595627112, -80.983546609059047)));
 
-        BusStopOverlay overlay = new BusStopOverlay(this, items, map);
+        BusStopOverlay overlay = new BusStopOverlay(parent.getContext(), items, map);
 
         map.getOverlays().add(overlay);
 
         IMapController mapController = map.getController();
         mapController.setZoom(20);
+        //Test start location in middle of sudbury
         GeoPoint startPoint = new GeoPoint(46.491271667182488, -80.988006619736623);
         mapController.setCenter(startPoint);
+
+        return view;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stops_map, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
